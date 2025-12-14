d0c724bb983cbf55253a75659c37c18e4cd1d631
Rustam Yunusov <yrustt@yandex.ru>
2025-12-14 08:55:15 +0300

    Рефакторинг

--- mast_parser/__main__.py
+++ mast_parser/__main__.py
@@ -7,7 +7,9 @@ from mast_parser.tasks import scrape, notify
 
 
 def main():
-    with daemon.DaemonContext(stdout=open("stdout.log", "a+"), stderr=open("stderr.log", "a+")):
+    with daemon.DaemonContext(
+        stdout=open("stdout.log", "a+"), stderr=open("stderr.log", "a+")
+    ):
         schedule.every().day.at("09:00").do(scrape)
         schedule.every().day.at("12:00").do(notify)
 
